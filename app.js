5022e42ecf8752ede72ac18a10c8ad65ec187c51
Thomas Desrosiers <tdesrosi@Thomass-Mac.local>
2020-07-11 14:58:06 -0500

    fixed app.js server

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get("/configure", function(req, res){
 });
 
 
-app.listen(3000, function() {
+app.listen(3000 || process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
